229928870c43df217d075fd9c40e7ab54c70deaf
Hesham <Hesham@Hesham-Pc>
2016-05-12 19:42:32 +0300

    Registers

--- src/mips/Registers.java
+++ src/mips/Registers.java
@@ -2,6 +2,7 @@
 package mips;
 
 public class Registers {
+    
    public static int $0  =0;
    public static int $at =0;
    public static int $v0 =0;
@@ -36,6 +37,100 @@ public class Registers {
    public static int $fp =0;
    public static int $ra =0;
    
+   public Registers(int[]Reg1, int[]Reg2,int[]WrtReg, int[]WrtData){
+       int LocReg1=Constants.BinToInt(Reg1);
+       int LocReg2=Constants.BinToInt(Reg2);
+
+       int ReadData1;
+       int ReadData2;
+        for (int i=0;i<=Constants.reg.length;i++){
+            if (i== LocReg1){
+            ReadData1=getValue(i);
+            }
+                
+        }
+        for (int i=0;i<=Constants.reg.length;i++){
+            if (i== LocReg2){
+            ReadData2=getValue(i);
+            }
+                
+        }
+   
+   }
+   public int getValue(int Loc){
+       for(int i=0;i<=Constants.reg.length;i++){
+       switch(Constants.reg[i]){
+           case "$0":
+               return $0;             
+            case"$at":
+               return $at;
+            case"$v0":
+                return $v0;
+            case"$v1":
+                return $v1;
+            case"$a0":
+                return $a0;
+            case"$a1":
+                return $a1;
+            case"$a2":
+                return $a2;
+            case "$a3":
+                return $a3;
+            case "$t0":
+                return $t0;
+            case"$t1":
+                return $t1;
+            case "$t2":
+                return $t2;
+            case "$t3":
+                return $t3;
+            case "$t4":
+                return $t3;
+            case "$t5":
+                return $t5;
+            case "$t6":
+                return $t6;
+            case "$t7":
+                return $t7;
+            case "$s0":
+                return $s0;
+            case "$s1":
+                return $s1;
+            case "$s2":
+                return $s2;
+            case "$s3":
+                return $s3;
+            case "$s4":
+                return $s4;
+            case "$s5":
+                return $s5;
+            case "$s6":
+                return $s6;
+            case "$s7":
+                return $s7;
+            case "$t8":
+                return $t8;
+            case"$t9":
+                return $t9;
+            case "$k0":
+                return $k0;
+            case "$k1":
+                return $k1;
+            case "$gp":
+                return $gp;
+            case "$sp":
+                return $sp;
+            case "$fp":
+                return $fp;
+            case "$ra":
+                return $ra;
+ 
+       }
+       
+       
+       }
+       return -1;
+   }
    
 
 }
