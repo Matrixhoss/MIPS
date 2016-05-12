47360a6a63beb19560817d10ef37c010e02e440d
Abdelrahman Ossama <doo2doo2@hotmail.com>
2016-05-12 18:25:14 +0200

    update to control unit

--- src/mips/ControlUnit.java
+++ src/mips/ControlUnit.java
@@ -4,20 +4,118 @@ package mips;
 import java.util.Arrays;
 
 public class ControlUnit {
-    public char[] input;
+    public String input;
     public boolean RegDest;
     public boolean Branch;
     public boolean MemRead;
     public boolean MemtoReg;
-    public int ALUOp;
+    public String ALUOp;
     public boolean ALUSrc;
     public boolean MemWrite;
     public boolean RegWrite;
+
+//        this.RegDest;
+//        this.Branch;
+//        this.MemRead;
+//        this.MemtoReg;
+//        this.ALUOp;
+//        this.ALUSrc;
+//        this.MemWrite;
+//        this.RegWrite;
+ControlUnit(){
     
-    ControlUnit(int[] input){
-        this.input= Arrays.toString(input).toCharArray();
+}
+ControlUnit(int[] input){
+    generateSignals(input);
+}
+    public void generateSignals(int[] input){
+        this.input= Arrays.toString(input);
         System.out.println(this.input);
-        char[] temp = {1,2,3,4,5,6};
-        //if(this.input.equals(1,2,3,4,5,6);
+        
+        switch(this.input.toString()){
+            case "000000":
+                //RFormat
+                break;
+            case "001000":
+                //addi
+                GenerateIFormatSignals();
+                break;
+            case "100011":
+                //Load instruction
+                GenerateLoadSingals();
+                break;
+            case "101011":
+                //save instruction
+                GenerateSaveSignals();
+                break;
+            case "000100":
+                //branch if equal
+                GenerateBeqSignals();
+                break;
+            case "100111":
+                //nor operation
+            case "000101":
+                //branch if not equal
+        }  
+    }
+    void GenerateRFormatSignals(){
+        this.RegDest=true;
+        this.Branch=false;
+        this.MemRead=false;
+        this.MemtoReg=false;
+        this.ALUOp="10";
+        this.ALUSrc=false;
+        this.MemWrite=false;
+        this.RegWrite=true;
+    }
+    void GenerateIFormatSignals(){
+        this.RegDest=false;
+        this.Branch=false;
+        this.MemRead=false;
+        this.MemtoReg=false;
+        //this.ALUOp;
+        this.ALUSrc=true;
+        this.MemWrite=false;
+        this.RegWrite=true;
+    }
+    void GenerateLoadSingals(){
+        this.RegDest=false;
+        this.Branch=false;
+        this.MemRead=true;
+        this.MemtoReg=true;
+        this.ALUOp="00";
+        this.ALUSrc=true;
+        this.MemWrite=true;
+        this.RegWrite=true;
+    }
+    void GenerateSaveSignals(){
+        this.RegDest=false;
+        this.Branch=false;
+        this.MemRead=false;
+        this.MemtoReg=true;
+        this.ALUOp="00";
+        this.ALUSrc=true;
+        this.MemWrite=false;
+        this.RegWrite=true;
+    }
+    void GenerateBeqSignals(){
+        this.RegDest=false;
+        this.Branch=true;
+        this.MemRead=false;
+        this.MemtoReg=true;
+        this.ALUOp="01";
+        this.ALUSrc=false;
+        this.MemWrite=false;
+        this.RegWrite=false;
+    }
+    void GenerateBneSignals(){
+//        this.RegDest=false;
+//        this.Branch=true;
+//        this.MemRead=false;
+//        this.MemtoReg=true;
+//        this.ALUOp="01";
+//        this.ALUSrc=false;
+//        this.MemWrite=false;
+//        this.RegWrite=false;
     }
 }
